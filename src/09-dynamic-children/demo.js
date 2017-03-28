8e0835e5ce2df48f3074e560795d3defb3219b2b
Miguel Ángel Durán <miguel@miduga.es>
2017-03-28 14:24:29 +0100

    Add dynamic children demo

---
+++ src/09-dynamic-children/demo.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import DynamicChildrenDemo from './index.js'
+
+export default <DynamicChildrenDemo initialShow={false} />
---
+++ src/09-dynamic-children/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class DynamicChildrenDemo extends Component {
+
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      show: this.props.initialShow,
+      images: [ 'https://unsplash.it/150?image=0','https://unsplash.it/150?image=1', 'https://unsplash.it/150?image=2','https://unsplash.it/150?image=3', 'https://unsplash.it/150?image=4' ]
+    }
+  }
+
+  showImage = () => {
+    this.setState({ show: true })
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          !this.state.show &&
+            <button onClick={this.showImage}>
+              Show me a bunch of images!
+            </button>
+        }
+
+        <div className={this.state.show ? 'show' : ''}>
+          {this.state.images.map((img, index) => {
+            return <img key={index} role='presentation' src={img} />
+          })}
+        </div>
+
+      </div>
+    );
+  }
+}
